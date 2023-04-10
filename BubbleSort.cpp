7ce9a4cbb0279b1d5fc7965d961fd6a1a9d28796
a1818136 <a1818136@adelaide.edu.au>
2023-04-11 02:02:28 +0930

    1

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 
 vector<int> BubbleSort::sort(vector<int> list){
-    int length = list.size();
+    vector<int> list1 = list;
+    int length = list1.size();
     int temp;
     for (int i=1; i<length; i++){
         for (int j=0; j<length-i; j++){
-            if (list.at(j) > list.at(j+1)){
-                list.at(j) = temp;
-                list.at(j) = list.at(j+1);
-                list.at(j+1) = temp;
+            if (list1.at(j) > list1.at(j+1)){
+                list1.at(j) = temp;
+                list1.at(j) = list1.at(j+1);
+                list1.at(j+1) = temp;
             }
         }
     }
+    return list1;
 }
---
+++ main.cpp
@@ -0,0 +1,6 @@
+# include "BubbleSort.h"
+# include "QuickSort.h"
+int main(){
+    BubbleSort a;
+    QuickSort b;
+}
