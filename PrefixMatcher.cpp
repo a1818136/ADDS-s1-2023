d588eb4098e01794e658f1bef3b94730474f76ab
a1818136 <a1818136@adelaide.edu.au>
2023-05-14 03:24:48 +0930

    3

--- PrefixMatcher.cpp
+++ PrefixMatcher.cpp
@@ -21,7 +21,8 @@ void PrefixMatcher::insert(string address, int routerNum){
 
 int PrefixMatcher::selectRouter(string networkAddress) {
     TrieNode* node = root;
-    for (int i=0; i<networkAddress.length() && node->children[networkAddress[i]-'0'] != nullptr; i++){
+    for (int i=0; i<(int)(networkAddress.length()); i++){
+        if (node->children[networkAddress[i]-'0'] == nullptr) break;
         node = node->children[networkAddress[i]-'0'];
     }
     return node->routerNumber;
