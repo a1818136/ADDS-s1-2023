7fa81519eb5422dd881745d26619aef69d77d457
a1818136 <a1818136@adelaide.edu.au>
2023-05-14 01:53:22 +0930

    2

--- Autocomplete.cpp
+++ Autocomplete.cpp
@@ -1,20 +1,50 @@
 # include "Autocomplete.h"
 using namespace std;
 
+#include <string>
+#include <vector>
+
+
 Autocomplete::Autocomplete(){
-    TrieNode* root = new TrieNode;
+    this->root = new TrieNode;
+    this->insert("bin");
+    this->insert("ball");
+    this->insert("ballet");
 }
 
 std::vector<std::string> Autocomplete::getSuggestions(std::string partialWord){
     TrieNode* curr = root;
-    for (auto c : partialWord){
-        if (curr->children){
-            
+    for (const auto c : partialWord){
+        int i = c-'a';
+        if (curr->children[i] == nullptr){
+            return {};
         }
+        curr = curr->children[i];
     }
-
+    std::vector<std::string> results;
+    searchAllTrie(curr, partialWord, results);
+    return results;
 }
 
-void Autocomplete::insert(std::string word){
+void Autocomplete::insert(const std::string word){
+    TrieNode* curr = root;
+    for (auto c : word){
+        int i = c-'a';
+        if (curr->children[i] == nullptr){
+            curr->children[i] = new TrieNode;
+        }
+        curr = curr->children[i];
+    }
+    curr->isEndOfWord = true;
+}
 
+void Autocomplete::searchAllTrie(const TrieNode* curr, const std::string& prefix, std::vector<std::string>& results){
+    if (curr->isEndOfWord == true){
+        results.push_back(prefix);
+    }
+    for (int i=0; i<26; i++){
+        if (curr->children[i] != nullptr){
+            searchAllTrie(curr->children[i], prefix+char('a'+i), results);
+        }
+    }
 }
--- Autocomplete.h
+++ Autocomplete.h
@@ -12,11 +12,11 @@ struct TrieNode{
 class Autocomplete{
 private:
     TrieNode* root;
+    void searchAllTrie(const TrieNode* curr, const std::string& prefix, std::vector<std::string>& results);
 public:
     Autocomplete();
     std::vector<std::string> getSuggestions(std::string partialWord);
     void insert(std::string word);
 };
-
 
 # endif
