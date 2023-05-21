c794f46c6efcdc97f5c909e49f91ce028cce15cb
a1818136 <a1818136@adelaide.edu.au>
2023-05-21 20:07:48 +0930

    1

--- klargest.cpp
+++ klargest.cpp
@@ -1,45 +1,16 @@
 #include <vector>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-#include <vector>
-
 int kth_largest(std::vector<int> values, int k) {
-  std::vector<int> minHeap;
-  for (int i = 0; i < values.size(); i++) {
-    minHeap.push_back(values[i]);
-    int index = minHeap.size() - 1;
-    while (index > 0 && minHeap[index] < minHeap[(index - 1) / 2]) {
-      std::swap(minHeap[index], minHeap[(index - 1) / 2]);
-      index = (index - 1) / 2;
+  priority_queue<int, vector<int>, greater<int>> prior_queue;
+  for (int value : values) {
+    prior_queue.push(value);
+
+    if (prior_queue.size() > k) {
+      prior_queue.pop();
     }
   }
-
-  int result = -1;
-  for (int i = 0; i < k; i++) {
-    if (minHeap.empty()) {
-      break; 
-    }
-    result = minHeap[0];
-    minHeap[0] = minHeap[minHeap.size() - 1];
-    minHeap.pop_back();
-    int index = 0;
-    while (true) {
-      int left_child = 2 * index + 1;
-      int right_child = 2 * index + 2;
-      int smallest = index;
-      if (left_child < minHeap.size() && minHeap[left_child] < minHeap[smallest]) {
-        smallest = left_child;
-      }
-      if (right_child < minHeap.size() && minHeap[right_child] < minHeap[smallest]) {
-        smallest = right_child;
-      }
-      if (smallest == index) {
-        break;
-      }
-      std::swap(minHeap[index], minHeap[smallest]);
-      index = smallest;
-    }
-  }
-
-  return result;
+  return prior_queue.top();
 }
