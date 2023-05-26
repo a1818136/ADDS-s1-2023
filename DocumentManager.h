63a289e4c45ced97a5cd64289c36a1c1c9512047
a1818136 <a1818136@adelaide.edu.au>
2023-05-26 21:03:25 +0930

    3

--- DocumentManager.h
+++ DocumentManager.h
@@ -23,9 +23,9 @@ public:
 
     void addPatron(int patronID);
 
-    int search(std::string name); // returns docid if name is in the document collection or 0 if the name is not in the collection
+    int search(std::string name); 
 
-    bool borrowDocument(int docid, int patronID);  // returns true if document is borrowed, false if it can not be borrowed (invalid patronid or the number of copies current borrowed has reached the license limit)
+    bool borrowDocument(int docid, int patronID); 
 
     void returnDocument(int docid, int patronID);
 
